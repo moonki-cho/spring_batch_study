aa3a8aa6950626e8d275f8b29c43b035bf7762d8
moonki-cho <jomoonki88@gmail.com>
2024-12-10 18:19:18 +0900

    charter 10 실습

---
+++ src/test/java/org/study/spring_batch_study/config/OnStepTaskJobConfigurationTest.java
@@ -0,0 +1,30 @@
+package org.study.spring_batch_study.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+
+@EnableAutoConfiguration
+@SpringBatchTest
+@SpringBootTest(classes = OnStepTaskJobConfiguration.class)
+class OnStepTaskJobConfigurationTest {
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Test
+	public void runJob() throws Exception {
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addLocalDateTime("currentLocalDataTime", LocalDateTime.now())
+				.toJobParameters();
+
+		jobLauncherTestUtils.launchJob(jobParameters);
+	}
+}
